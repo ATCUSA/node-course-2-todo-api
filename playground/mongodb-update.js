f06e5e4fcedd2afbe52c4ee3604561feed0838d0
Austin Cole <mail@acole.us>
2018-03-20 14:25:10 -0700

    Add update script.

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+  const db = client.db('TodoApp')
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: ObjectID("5ab0105717dc0a151ca6962a")
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: ObjectID("5aafe294e0c1ec43377a43d6")
+  }, {
+    $set: {
+      name: "Austin"
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // client.close();
+});
